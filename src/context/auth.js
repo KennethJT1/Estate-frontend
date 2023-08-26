26f869953b101066ce343fae52d17e9d0cff7877
Kenneth <deca@DECAGONs-MacBook-Air.local>
2023-08-26 03:10:59 +0100

    added config.js

--- src/context/auth.js
+++ src/context/auth.js
@@ -1,6 +1,7 @@
 import { useState, createContext, useContext, useEffect } from "react";
 import axios from "axios";
 import { API } from "../config";
+console.log("data==>", API)
 
 const AuthContext = createContext();
 
